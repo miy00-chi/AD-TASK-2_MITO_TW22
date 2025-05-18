9ec4609dee7b94d52bf7d72070095463fd2c3b2f
Hazel Mito <hazelmito2029@gmail.com>
2025-05-18 19:47:37 +0800

    - Added CSS in landing page

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AD-TASK2_MITO_TW22</title>
+    <link rel="stylesheet" href="assets\css\style.css">
 </head>
 <body>
     <div class="container">
