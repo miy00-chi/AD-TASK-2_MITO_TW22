73541963fad7099012e6e92f21b20403d5c6d10e
Hazel Mito <hazelmito2029@gmail.com>
2025-05-18 18:09:08 +0800

    Add initial implementation of book management system
    - Created example.js and index.php as placeholders in the pageLibrary directory.
    - Implemented router.php to handle incoming requests and serve the main index file.
    - Developed Book.utils.php with functions for managing a book catalog, including adding books, printing the catalog, and listing authors.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AD-TASK2_MITO_TW22</title>
+</head>
+<body>
+    <div class="container">
+        <h1>WELCOME TO YOUR LIBRARY</h1>
+        <h4>Escape your fantasies for the day</h4>
+        <div class="link">
+            <a href="page\pageLibrary\index.php">Start</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ utils/Book.utils.php
@@ -0,0 +1,45 @@
+<?php
+    function book($id, $title, $author, $year){
+        return[
+            "id" => $id,
+            "title" => $title,
+            "author" => $author,
+            "year" => $year
+        ];
+    }
+
+    function addBook($catalogue, $book){
+        $catalogue[$book['id']] = book;
+    }
+
+    function printCatalogue($catalogue){
+        echo "<h2>Books: </h2><ul>";
+        foreach($catalogue as $book){
+            echo "<li>
+                ID: {$book['id']}, 
+                Title: {$book['title']}, 
+                Author: {$book['author']}, 
+                Year: {$book['year']}
+            </li>";
+        }
+        echo "</ul>";
+    }
+
+    function getAuthorList($catalogue){
+        $authors = [];
+        foreach($catalogue as $book){
+            $authors[] = $book['author'];
+        }
+        return $authors;
+    }
+
+    function printAuthorList($authors){
+        echo "<h3>Authors List: </h3><ul>";
+        foreach ($authors as $author){
+            echo "<li>$author</li>";
+        }
+        echo "</ul>";
+    }
+
+
+?>
